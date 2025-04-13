169e9116ba7e17be6c739fd08a30965b37ce28ef
Tom Mount <tmountjr@gmail.com>
2025-04-13 19:57:12 -0400

    tighten up process of fetching votes

--- src/app/nominations/[id]/page.tsx
+++ src/app/nominations/[id]/page.tsx
@@ -7,102 +7,71 @@ export default async function Page({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  const votes = (await lawmakerVotesByNomination(id)).reduce(
-    (acc, curr) => {
-      const position = curr.position && (curr.position == "Yea" || curr.position == "Nay") ? curr.position.toLowerCase() : 'unknown';
-      if (curr.party?.toLowerCase() === "d") {
-        acc.d[position].push(curr)
-      } else if (curr.party?.toLowerCase() === "r") {
-        acc.r[position].push(curr);
-      } else {
-        acc.other[position].push(curr);
-      }
-      return acc;
-    },
-    {
-      d: { yea: [], nay: [], unknown: [] },
-      r: { yea: [], nay: [], unknown: [] },
-      other: { yea: [], nay: [], unknown: [] },
-    }
-  );
+
+  const [ dVotes, rVotes, otherVotes ] = await Promise.all([
+    lawmakerVotesByNomination(id, 'd'),
+    lawmakerVotesByNomination(id, 'r'),
+    lawmakerVotesByNomination(id, 'other'),
+  ])
+
+  // Internal component to make the display a little cleaner
+  const partyTable = (party: string, votes: any[]) => {
+    const grouped = votes.reduce((acc, curr) => {
+      const position = curr.position && (curr.position === 'Yea' || curr.position === 'Nay')
+        ? curr.position.toLowerCase()
+        : 'unknown';
+      
+      acc[position].push(curr)
+      return acc
+    }, { yea: [], nay: [], unknown: [] })
+
+    const partyName = party === 'd'
+      ? 'Democratic Party'
+      : party === 'r'
+        ? 'Republican Party'
+        : 'Other'
+    
+    return (
+      <div className="flex flex-col">
+        <h2>{partyName}</h2>
+        <div className="flex flex-row gap-10">
+          <div className="flex flex-col">
+            <h3>Yea</h3>
+            {grouped.yea.map((v) => (
+              <p key={v.legislatorId}>
+                {v.name} ({v.state})
+              </p>
+            ))}
+          </div>
+          <div className="flex flex-col">
+            <h3>Nay</h3>
+            {grouped.nay.map((v) => (
+              <p key={v.legislatorId}>
+                {v.name} ({v.state})
+              </p>
+            ))}
+          </div>
+          <div className="flex flex-col">
+            <h3>Other</h3>
+            {grouped.unknown.map((v) => (
+              <p key={v.legislatorId}>
+                {v.name} ({v.state}) voted &quot;{v.position}&quot;
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+    )
+  };
   
   return (
     <>
       <PageHeader title={`Nomination #${id}`} />
       <section className="flex flex-row gap-10">
-        <div>
-          <h2>Democratic Party</h2>
-          <div className="flex flex-row gap-10">
-            <div className="flex flex-col">
-              <h3>Yea</h3>
-              {votes.d.yea.map(v => (
-                <p key={v.legislatorId}>{v.name} ({v.state})</p>
-              ))}
-            </div>
-            <div className="flex flex-col">
-              <h3>Nay</h3>
-              {votes.d.nay.map(v => (
-                <p key={v.legislatorId}>{v.name} ({v.state})</p>
-              ))}
-            </div>
-            <div className="flex flex-col">
-              <h3>Other</h3>
-              {votes.d.unknown.map(v => (
-                <p key={v.legislatorId}>{v.name} ({v.state}) voted "{v.position}"</p>
-              ))}
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <h2>Republican Party</h2>
-          <div className="flex flex-row gap-10">
-            <div className="flex flex-col">
-              <h3>Yea</h3>
-              {votes.r.yea.map(v => (
-                <p key={v.legislatorId}>{v.name} ({v.state})</p>
-              ))}
-            </div>
-            <div className="flex flex-col">
-              <h3>Nay</h3>
-              {votes.r.nay.map(v => (
-                <p key={v.legislatorId}>{v.name} ({v.state})</p>
-              ))}
-            </div>
-            <div className="flex flex-col">
-              <h3>Other</h3>
-              {votes.r.unknown.map(v => (
-                <p key={v.legislatorId}>{v.name} ({v.state}) voted "{v.position}"</p>
-              ))}
-            </div>
-          </div>
-        </div>
-        
-        <div>
-          <h2>Other</h2>
-          <div className="flex flex-row gap-10">
-            <div className="flex flex-col">
-              <h3>Yea</h3>
-              {votes.other.yea.map(v => (
-                <p key={v.legislatorId}>{v.name} ({v.party}, {v.state})</p>
-              ))}
-            </div>
-            <div className="flex flex-col">
-              <h3>Nay</h3>
-              {votes.other.nay.map(v => (
-                <p key={v.legislatorId}>{v.name} ({v.party}, {v.state})</p>
-              ))}
-            </div>
-            <div className="flex flex-col">
-              <h3>Other</h3>
-              {votes.other.unknown.map(v => (
-                <p key={v.legislatorId}>{v.name} ({v.party}, {v.state}) voted "{v.position}"</p>
-              ))}
-            </div>
-          </div>
-        </div>
-        
+        {partyTable('d', dVotes)}
+        {partyTable('r', rVotes)}
+        {partyTable('other', otherVotes)}
       </section>
     </>
-  )
+  );
 }
--- src/db/queries/nominations.ts
+++ src/db/queries/nominations.ts
@@ -4,7 +4,7 @@ import {
   legislators as l,
   latestVoteIds as vm,
 } from '@/db/schema';
-import { asc, eq } from 'drizzle-orm';
+import { and, asc, eq, inArray, not } from 'drizzle-orm';
 
 // Subquery select to filter for just nomination votes
 const nominationVotes = db
@@ -37,15 +37,44 @@ const _nominationVoteIds = db
   /**
    * Get all individual votes for a single vote.
    * @param voteId The Vote ID.
+   * @param party  If provided, limit return to a specific party's voters.
    * @returns The nominationVote object for the given vote.
    */
-export const lawmakerVotesByNomination = async (voteId: string) => {
-  return db
-    .select()
-    .from(nominationVotes)
-    .where(eq(nominationVotes.voteId, voteId))
-    .orderBy(asc(nominationVotes.party))
-    .execute()
+export const lawmakerVotesByNomination = async (voteId: string, party: string = 'all') => {
+  if (party === 'all') {
+    return db
+      .select()
+      .from(nominationVotes)
+      .where(eq(nominationVotes.voteId, voteId))
+      .orderBy(asc(nominationVotes.party))
+  } else if (party === 'd') {
+    return db
+      .select()
+      .from(nominationVotes)
+      .where(and(
+        eq(nominationVotes.voteId, voteId),
+        eq(nominationVotes.party, 'D')
+      ))
+      .orderBy(asc(nominationVotes.party))
+  } else if (party === 'r') {
+    return db
+      .select()
+      .from(nominationVotes)
+      .where(and(
+        eq(nominationVotes.voteId, voteId),
+        eq(nominationVotes.party, 'D')
+      ))
+      .orderBy(asc(nominationVotes.party))
+  } else {
+    return db
+      .select()
+      .from(nominationVotes)
+      .where(and(
+        eq(nominationVotes.voteId, voteId),
+        not(inArray(nominationVotes.party, ['R', 'D']))
+      ))
+      .orderBy(asc(nominationVotes.party))
+  }
 }
 
 export const nominationVoteIds = _nominationVoteIds.execute()
