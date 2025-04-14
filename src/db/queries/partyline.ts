90846a98cdc94b15fcb7700d4e7e6189fee5da4c
Tom Mount <tmountjr@gmail.com>
2025-04-14 15:06:28 -0400

    overhaul party lines stored queries

--- src/db/queries/partyline.ts
+++ src/db/queries/partyline.ts
@@ -1,5 +1,5 @@
 import { db } from "@/db";
-import { desc, eq, sql } from "drizzle-orm";
+import { and, asc, count, desc, eq, inArray, sql } from "drizzle-orm";
 import {
   bills as b,
   votes as v,
@@ -8,19 +8,19 @@ import {
   latestVoteIds as v_ids,
 } from "@/db/schema";
 
-// First get the 10 most recent votes.
-const recentVotes = db
+const _tempVoteMeta = db
   .select({
+    voteNumber: v_ids.voteNumber,
     voteId: v_ids.voteId,
     billId: v_ids.billId,
     chamber: v_ids.chamber,
     date: v_ids.date,
     result: v_ids.result,
     category: v_ids.category,
-    nominationTitle: v_ids.nominationTitle,
-    title: b.title,
+    nomination_title: v_ids.nominationTitle,
+    sourceFilename: v_ids.sourceFilename,
     sponsorParty: sql<string>`
-      CASE
+     CASE
         WHEN ${v_ids.category} = 'nomination'
         THEN 'R'
         ELSE ${vm.sponsorParty}
@@ -29,54 +29,79 @@ const recentVotes = db
   .from(v_ids)
   .innerJoin(vm, eq(v_ids.voteId, vm.voteId))
   .leftJoin(b, eq(v_ids.billId, b.billId))
-  .orderBy(desc(v_ids.date))
-  .limit(10)
-  .as("recent_votes");
+  .as('temp_vote_meta');
 
-// Now fetch all vote details pertaining to those votes and determine if it's
-// a party line vote or not.
-// A party line vote is determined by:
-//   legislator_party = sponsor_party and position = "yea"
-const allVotes = db
+const _votesWithPartyLine = db
   .select({
     voteId: v.voteId,
     legislatorId: v.legislatorId,
     position: v.position,
-    billId: recentVotes.billId,
-    sponsorParty: recentVotes.sponsorParty,
-    date: recentVotes.date,
-    category: recentVotes.category,
-    nominationTitle: recentVotes.nominationTitle,
-    title: recentVotes.title,
+    sponsor_party: _tempVoteMeta.sponsorParty,
     name: l.name,
+    state: l.state,
+    district: l.district,
+    termType: l.termType,
     party: l.party,
     isPartyLine: sql<boolean>`
       CASE
-        WHEN (${l.party} = ${recentVotes.sponsorParty} AND ${v.position} != 'Nay') 
-          OR (${l.party} != ${recentVotes.sponsorParty} AND ${v.position} != 'Yea')
+        WHEN (${l.party} =  ${_tempVoteMeta.sponsorParty} AND ${v.position} != 'Nay') 
+          OR (${l.party} != ${_tempVoteMeta.sponsorParty} AND ${v.position} != 'Yea')
         THEN TRUE
         ELSE FALSE
       END`.as("is_party_line")
   })
   .from(v)
-  .innerJoin(recentVotes, eq(v.voteId, recentVotes.voteId))
+  .innerJoin(_tempVoteMeta, eq(v.voteId, _tempVoteMeta.voteId))
   .innerJoin(l, eq(v.legislatorId, l.id))
-  .as("all_votes");
+  .as("votes_with_party_line")
 
-// Finally, summarize those votes by how many did or did not vote along party lines.
-export const data = await db
+const _brokePartyLineVotes = db
   .select({
-    voteId: allVotes.voteId,
-    billId: allVotes.billId,
-    date: allVotes.date,
-    category: allVotes.category,
-    title: sql<string>`CASE WHEN ${allVotes.category} = 'nomination' THEN ${allVotes.nominationTitle} ELSE ${allVotes.title} END`.as('title'),
-    rPartyLine: sql<boolean>`SUM(CASE WHEN ${allVotes.party} = 'R' AND ${allVotes.isPartyLine} = 1 THEN 1 ELSE 0 END)`.as("r_party_line"),
-    rNotPartyLine: sql<boolean>`SUM(CASE WHEN ${allVotes.party} = 'R' AND ${allVotes.isPartyLine} = 0 THEN 1 ELSE 0 END)`.as("r_not_party_line"),
-    dPartyLine: sql<boolean>`SUM(CASE WHEN ${allVotes.party} = 'D' AND ${allVotes.isPartyLine} = 1 THEN 1 ELSE 0 END)`.as("d_party_line"),
-    dNotPartyLine: sql<boolean>`SUM(CASE WHEN ${allVotes.party} = 'D' AND ${allVotes.isPartyLine} = 0 THEN 1 ELSE 0 END)`.as("d_not_party_line")
+    legislatorId: _votesWithPartyLine.legislatorId,
+    name: _votesWithPartyLine.name,
+    state: _votesWithPartyLine.state,
+    district: _votesWithPartyLine.district,
+    termType: _votesWithPartyLine.termType,
+    party: _votesWithPartyLine.party,
+    brokePartyLineCount: sql<number>`
+      CASE
+        WHEN ${_votesWithPartyLine.isPartyLine} = 0
+        THEN 1
+        ELSE 0
+      END`.as('broke_party_line_count'),
+    totalVoteCount: count(_votesWithPartyLine.legislatorId).as("total_vote_count"),
   })
-  .from(allVotes)
-  .groupBy(allVotes.voteId)
-  .orderBy(desc(allVotes.date))
-  .execute();
+  .from(_votesWithPartyLine)
+  .groupBy(_votesWithPartyLine.legislatorId)
+  .as('broke_party_line_votes');
+
+interface BrokePartyLinesFilters {
+  state?: string;
+  chamber?: "sen" | "rep";
+  party?: "d" | "r" | "i";
+  legislatorIds?: string[];
+}
+
+/**
+ * Get the Broke Party Lines data, optionally limited by a set of filters.
+ * @param param0.state Limit results by state.
+ * @param param0.chamber Limit results by chamber. Use "sen" or "rep" for Senate or House.
+ * @param param0.party Limit results by legislator party. Use "d", "r", or "i".
+ * @param param0.legislatorIds Limit results by one or more legislator IDs.
+ * @returns The Broke Party Lines summaries for the given filters.
+ */
+export const brokePartyLineVotes = ({
+  state, chamber, party, legislatorIds
+}: BrokePartyLinesFilters) => db
+  .select()
+  .from(_brokePartyLineVotes)
+  .where(
+    and(
+      state ? eq(_brokePartyLineVotes.state, state) : undefined,
+      chamber ? eq(_brokePartyLineVotes.termType, chamber) : undefined,
+      party ? eq(_brokePartyLineVotes.party, party.toUpperCase()) : undefined,
+      legislatorIds ? inArray(_brokePartyLineVotes, legislatorIds) : undefined
+    )
+  )
+  .orderBy(asc(_votesWithPartyLine.party), desc(_brokePartyLineVotes.brokePartyLineCount))
+  .execute()
