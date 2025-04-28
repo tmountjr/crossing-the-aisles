d505efd189e627a2a1c5515985bbdedfddd87412
Tom Mount <tmountjr@gmail.com>
2025-04-28 10:14:31 -0400

    better cookie handling and code centralization; add a "find me" icon to reset state detection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,24 +8,19 @@ import VoteBarChart from "./components/VoteBarChart";
 import { AllowedChambers } from "@/db/queries/partyline";
 import { AllowedParties } from "@/db/queries/legislators";
 import LegislatorList from "@/app/components/LegislatorList";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlassLocation } from "@fortawesome/free-solid-svg-icons";
 
 /**
  * Get the state code of the client from ipinfo, if available.
  * @returns The two-letter state code for US states, if available, otherwise an empty string.
  */
 const getClientState = async (): Promise<string> => {
-  // Check if there's already a cookie for geo, and if so, use the state it provides.
-  const state = Cookies.get("geo");
-  if (state) {
-    return state;
-  }
-
   const response = await fetch("https://ipinfo.io/json");
   const data = await response.json();
   if (data.country && data.country === "US" && data.region) {
     const foundState = states.find((s) => s.name === data.region);
     if (foundState) {
-      Cookies.set("geo", foundState.code, { path: "/" });
       return foundState.code;
     }
   }
@@ -40,15 +35,25 @@ export default function Home() {
 
   // On page load, set the state if we can.
   useEffect(() => {
-    getClientState().then((code) => {
-      setSelectedState(code);
-    });
+    const stateCode = Cookies.get("geo");
+    if (stateCode) {
+      setSelectedState(stateCode);
+      return;
+    }
+    doGeoLocation();
   }, []);
 
   const handleSelectedStateChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const code = e.target.value;
-    Cookies.set("geo", code, { path: "/" });
+    Cookies.set("geo", code);
     setSelectedState(e.target.value);
+  };
+
+  const doGeoLocation = () => {
+    getClientState().then((code) => {
+      Cookies.set("geo", code);
+      setSelectedState(code);
+    });
   };
 
   const chamberFilterList: {
@@ -98,6 +103,15 @@ export default function Home() {
               </option>
             ))}
           </select>
+          <span
+            className="cursor-pointer"
+            onClick={doGeoLocation}
+          >
+            <FontAwesomeIcon
+              icon={faMagnifyingGlassLocation}
+              className="fa fa-fw text-2xl"
+            />
+          </span>
         </div>
 
         {/* Row 2 of the filters is all the other stuff - chamber (house, senate) and party (R, D) */}
